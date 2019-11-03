2877cc02ebd9fa6a00f563dc4824e8779cac237d
Brett Porter <gkdesign06@gmail.com>
2019-11-02 23:44:22 -0500

    more changes to layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,7 @@ $('.about-btn').on('click',function(event){
     event.preventDefault();
     window.location.href = '#about';
 })
+
+$('.page-links').on('click', 'li', function(){
+    console.log(this.getAttribute('data-scroll'));
+})
